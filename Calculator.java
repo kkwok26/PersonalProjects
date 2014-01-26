d19782ca7ee703651129509a3a452186d1f7b177
Kristine Kwok <kristinekkwok@gmail.com>
2014-01-25 21:05:34 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
 
-	public static void main(String args[]) {
+	public static void main( String args[] )  {
 	
   	Scanner keyboard = new Scanner(System.in);
   	double firstNumber;
